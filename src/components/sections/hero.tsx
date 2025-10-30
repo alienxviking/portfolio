642e57feb89f54c67cb7147a67f4f9958235833e
alienxviking <sroy191006@gmail.com>
2025-10-31 00:35:10 +0530

    minor changes

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ const HeroSection = () => {
               <div className="mt-8 md:ml-2 flex flex-col gap-3">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1KfIg-N9dyh9hP0ZC1hX38hOJ_GIWvG4Y/view?usp=sharing"
+                    "https://drive.google.com/file/d/1KMZcGw-14hYapQnJvTH3QLXq2ulwascb/view?usp=sharing"
                   }
                   target="_blank"
                   className="flex-1"
